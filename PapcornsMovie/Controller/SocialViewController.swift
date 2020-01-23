9281abbbcc950c0615828ff8271a2eb634199ed8
Yaser Bahrami <yaserbahrami@outlook.com>
2020-01-23 20:55:28 +0300

    init view controllers

---
+++ PapcornsMovie/Controller/SocialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SocialViewController.swift
+//  PapcornsMovie
+//
+//  Created by yaser on 1/23/20.
+//  Copyright © 2020 Bahrami. All rights reserved.
+//
+
+import UIKit
+
+class SocialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
